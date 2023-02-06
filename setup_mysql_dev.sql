f7c798437410b7b62208bdabb3910a7042cfea61
Dfunn1k <danychavezdev@gmail.com>
2023-02-06 16:44:12 -0500

    ci: added new sql script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,15 +3,3 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED "hbnb_dev_pwd";
 GRANT ALL ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"hbnb_dev";
-
-
-
--- MySQL setup development
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-
-
-
